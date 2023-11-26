9b5d07fef9fb93012386eba2114410a31721755b
vazw <4wonzest@gmail.com>
2023-11-26 21:02:09 +0700

    added server core api

--- src/app/core_api/api.rs
+++ src/app/core_api/api.rs
@@ -19,33 +19,58 @@ pub struct BoolRespons {
     pub status: i8,
 }
 
+#[cfg(feature = "ssr")]
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq, Eq)]
+pub struct UsersData {
+    pub id: String,
+    pub name: String,
+    pub pubkey: String,
+    pub lightning_url: String,
+    pub created: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct UsersDataResult {
+    pub id: String,
+    pub name: String,
+    pub pubkey: String,
+    pub lightning_url: String,
+    pub created: String,
+}
+
 #[server]
-pub async fn check_npub(hex_npub: String) -> Result<BoolRespons, ServerFnError> {
+pub async fn check_npub(hex_npub: String) -> Result<UsersDataResult, ServerFnError> {
     let mut con = db().await.unwrap();
     let query = format!("SELECT * FROM users WHERE pubkey='{hex_npub}'");
-    match sqlx::query_as::<_, UsersData>(&query).fetch_one(&mut con).await {
-        Ok(_user) => Ok(BoolRespons{
-            status:1
+    let result = sqlx::query_as::<_, UsersData>(&query).fetch_one(&mut con).await;
+    match result {
+        Ok(user) => Ok(UsersDataResult{
+            id:user.id,
+            name:user.name,
+            pubkey:user.pubkey,
+            lightning_url:user.lightning_url,
+            created:user.created,
         }),
-        Err(_) => Ok(BoolRespons{
-            status:0
-        }),
+        Err(e) => Err(e.into()),
     }
 }
 
 #[server]
 pub async fn check_username(username: String) -> Result<BoolRespons, ServerFnError> {
     let mut con = db().await.unwrap();
-    let query = format!("SELECT * FROM users WHERE name='{username}'");
-    let result = sqlx::query_as::<_,UsersData>(&query).fetch_one(&mut con).await;
-    println!("{:#?}", result);
-    match result {
-        Ok(_user) => Ok(BoolRespons{
-            status:1
-        }),
-        Err(_) => Ok(BoolRespons{
-            status:0
-        }),
+    if username.is_empty() {
+        Ok(BoolRespons { status: 0 })
+    } else {
+        let query = format!("SELECT * FROM users WHERE name='{username}'");
+        let result = sqlx::query_as::<_,UsersData>(&query).fetch_one(&mut con).await;
+        match result {
+            Ok(_user) => Ok(BoolRespons{
+                status:1
+            }),
+            Err(_) => Ok(BoolRespons{
+                status:0
+            }),
+        }
     }
 }
 
@@ -72,16 +97,59 @@ pub async fn add_user(username: String, pubkey: String, lnurl: String) -> Result
     }
 }
 
-
-#[cfg(feature = "ssr")]
-#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq, Eq)]
-pub struct UsersData {
-    pub id: String,
-    pub name: String,
-    pub pubkey: String,
-    pub lightning_url: String,
-    pub created: String,
+#[server]
+pub async fn edit_user(username: String, pubkey: String, lnurl: String) -> Result<BoolRespons, ServerFnError> {
+    let mut con = db().await.unwrap();
+    let query = format!("UPDATE users SET name='{username}', lightning_url='{lnurl}' WHERE pubkey='{pubkey}'");
+    match sqlx::query(&query)
+            .execute(&mut con)
+            .await
+             {
+        Ok(_user) => Ok(BoolRespons{
+            status:1
+        }),
+        Err(_) => Ok(BoolRespons{
+            status:0
+        }),
+    }
 }
+
+#[server]
+pub async fn delete_user(pubkey: String) -> Result<BoolRespons, ServerFnError> {
+    let mut con = db().await.unwrap();
+    match sqlx::query("DELETE FROM users WHERE pubkey=(?)")
+            .bind(pubkey)
+            .execute(&mut con)
+            .await
+             {
+        Ok(_user) => Ok(BoolRespons{
+            status:1
+        }),
+        Err(_) => Ok(BoolRespons{
+            status:0
+        }),
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
